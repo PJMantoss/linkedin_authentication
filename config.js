aae8d4d65d8cb1be7e5ec034ac1fb952379fb55b
pjmantoss <joelptoss@gmail.com>
2021-02-18 19:26:56 +0100

    update config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'linkedInAuth': {
-        'clientID': '',
-        'clientSecret': '',
-        'callbackURL': ''
+        'clientID': '7794jm5gu33dld',
+        'clientSecret': 'bJdp4G3Ztsn9ZRtY',
+        'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
     }
 }
